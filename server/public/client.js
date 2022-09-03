648386b9ca0b4b0fc071da2f40ad393a3b0d85a4
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-03 12:55:15 -0500

    create ajax get request

--- server/public/client.js
+++ server/public/client.js
@@ -1,7 +1,35 @@
-console.log('js');
-	
 $(document).ready(onReady);
 
 function onReady(){
-console.log('JQ')
+    $('#equalButton').on('click', getArray)
 }
+
+function getArray(){
+    console.log('in getarray');
+    $.ajax({
+        method: 'GET',
+        url: '/array'
+    }).then(function(array) {
+        console.log('got a response');
+        let currentAnswer = array[0].answer;
+        $('#answer').empty();
+        $('#answer').append(currentAnswer);
+        $('.calcHistory').empty();
+        for (let i = 1; i < array.length; i++) {
+            let histNum1 = array[i].num1;
+            let histAction = array[i].action;
+            let histNum2 = array[i].num2;
+            let histAnswer = array[i].answer;
+            $('.calcHistory').append(`<li>${histNum1} ${histAction} ${histNum2} = ${histAnswer}</li>`);
+        }
+    }) 
+        
+    }
+// GET REQUEST:
+// ✅ make equals button respond to clicks
+// ✅ FE: send GET request for the data
+// ✅ BE: create route on server to receive the GET request
+// ✅ BE: new route will send an array of objects as its response
+// FE: when the array arrives: 
+    //1) ✅ for the 0 index of the array, grab the .answer value and display to the DOM
+    //2) ✅ for the 1 - array.length index of array, loop through and append a list to the dom.
--- server/server.js
+++ server/server.js
@@ -1,12 +1,56 @@
 	// Importing the express node module:
-	const express=require('express');
+	const express = require('express');
 	// Using express to create an instance of an app (or server).
-	const app=express();
+	const app = express();
 	// Create a variable whose value is the port address.
-	const PORT=5000;
+	const PORT = 5000;
 	// Tell our server where the static assets live:
 	app.use(express.static('server/public'));
+	
+	let array = [
+		{
+			num1: 2,
+			action: '+',
+			num2: 4,
+			answer: 6,
+		},
+		{
+			num1: 5,
+			action: '-',
+			num2: 3,
+			answer: 2,
+		},
+		{
+			num1: 7,
+			action: '*',
+			num2: 8,
+			answer: 56,
+		},
+		{
+			num1: 12,
+			action: '/',
+			num2: 3,
+			answer: 4,
+		}
+	];
+	
+	
+	app.get('/array', (req, res) => {
+		console.log('/array got a request');
+		res.send(array);
+	})
+	
+	
+
+
+
+
+
+
+	
+	
 	// Start the server:
 	app.listen(PORT,()=>{
-	  console.log(`server is listening for requests on port ${PORT}.`)
+	  console.log(`server is listening for requests on port ${PORT}.`);
 })
+
